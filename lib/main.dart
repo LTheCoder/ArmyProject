4d80be0fb986aa752cec5dab5a2ddea0c013332f
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2022-07-27 10:21:41 +0300

    titles changed

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Army Project',
       theme: ThemeData(
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Army Project'),
     );
   }
 }
